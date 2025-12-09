2d7638c67fe86ef344cf7c13a88699b14191879d
Maciej Mensfeld <maciej@mensfeld.pl>
2025-12-09 11:38:51 +0100

    Remove Ruby 3.5/4.0 IRB notifier shim
    The IRB notifier shim is no longer needed because:
    1. yard-lint uses YARD's modern Ripper-based parser
    2. The legacy parser (which depends on irb/notifier) is only loaded
       when explicitly using the :ruby18 parser type

--- lib/yard-lint.rb
+++ lib/yard-lint.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
-# Load IRB notifier shim before YARD to avoid IRB dependency in Ruby 3.5+
-# This must be loaded before any YARD code is required
-require_relative 'yard/lint/ext/irb_notifier_shim'
-
 require 'zeitwerk'
 
 # Setup Zeitwerk loader for gem
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.ignore(__FILE__)
-loader.ignore("#{__dir__}/yard/lint/ext")
 loader.ignore("#{__dir__}/yard/lint/version.rb")
 loader.setup
 
